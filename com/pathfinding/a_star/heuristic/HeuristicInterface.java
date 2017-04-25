09fc272929381570d96421b60f83fbb1e9c889bf
Brian Sampayan <brian.sampayan@asu.edu>
2017-04-24 23:03:21 -0700

    Added Heuristics to be used by A* pathfinder

--- com/pathfinding/a_star/heuristic/HeuristicInterface.java
+++ com/pathfinding/a_star/heuristic/HeuristicInterface.java
@@ -2,4 +2,5 @@ package com.pathfinding.a_star.heuristic;
 
 public interface HeuristicInterface {
     float getCost(int x, int y, int tX, int tY);
+    float getCost(int x, int y, int sX, int sY, int tX, int tY);
 }
--- com/pathfinding/a_star/heuristic/ManhattanHeuristic.java
+++ com/pathfinding/a_star/heuristic/ManhattanHeuristic.java
@@ -2,11 +2,32 @@ package com.pathfinding.a_star.heuristic;
 
 public class ManhattanHeuristic implements HeuristicInterface {
     @Override
-    public float getCost(int x, int y, int tX, int tY) {
+    public float getCost(int x, int y, int sX, int sY, int tX, int tY) {
+        float dx1 = x - tX;
+        float dy1 = y - tY;
+        float dx2 = sX - tX;
+        float dy2 = sY - tY;
+        float cross = Math.abs(dx1*dy2 - dx2*dy1);
+
         float dx = Math.abs(x - tX);
         float dy = Math.abs(y - tY);
         float D = 1.0f;
 
-        return D * (dx + dy);
+        float heuristic = D * (dx + dy);
+
+
+        heuristic *= cross*0.001;
+
+        return heuristic;
+    }
+
+    @Override
+    public float getCost(int x, int y, int tX, int tY)
+    {
+        float dx = Math.abs(x - tX);
+        float dy = Math.abs(y - tY);
+        float D = 1.0f;
+
+        return (D * (dx + dy));
     }
 }
