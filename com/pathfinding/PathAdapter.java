d2f61c2230c9ac739035f0fd188b902242d25716
Brian Sampayan <brian.sampayan@asu.edu>
2017-05-03 23:27:28 -0700

    Added print functionality to adapter, and made functions static

--- com/pathfinding/PathAdapter.java
+++ com/pathfinding/PathAdapter.java
@@ -24,7 +24,7 @@ public final class PathAdapter
         return instance;
     }
 
-    public byte[] toByteArray(byte[] initArray, int graphWidth, int graphHeight, Path path, byte pathValue)
+    public static byte[] toByteArray(byte[] initArray, int graphWidth, int graphHeight, Path path, byte pathValue)
     {
         byte[] result = initArray.clone();
         if (path != null)
@@ -37,7 +37,7 @@ public final class PathAdapter
                     int index = step.getY() * graphHeight + step.getX();
                     if (isOutOfBounds(step.getX(), step.getY(), graphWidth, graphHeight))
                     {
-                        throw new ArrayIndexOutOfBoundsException("Index: " + index + " occurs outside the bounds of the graph.");
+                        throw new ArrayIndexOutOfBoundsException("[Error:toByteArray]Index: " + index + " occurs outside the bounds of the graph.");
                     }
                     result[index] = pathValue;
                 }
@@ -51,7 +51,31 @@ public final class PathAdapter
         return result;
     }
 
-    private boolean isOutOfBounds(int x, int y, int width, int height)
+    public static void printByteArray(byte[] graph, int graphWidth, int graphHeight, java.io.PrintStream out)
+    {
+        try
+        {
+            for (int j = 0; j < graphHeight; ++j)
+            {
+                for (int i = 0; i < graphWidth; ++i)
+                {
+                    int index = j * graphHeight + i;
+                    if (isOutOfBounds(i, j, graphWidth, graphHeight))
+                    {
+                        throw new ArrayIndexOutOfBoundsException("[Error:PrintByteArray]Index: " + index + " occurs outside the bounds of the graph.");
+                    }
+                    out.print(graph[index] + " ");
+                }
+                out.println();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace(out);
+        }
+    }
+
+    private static boolean isOutOfBounds(int x, int y, int width, int height)
     {
         return ((x < 0 || x >= width) || (y < 0 || y >= height));
     }
